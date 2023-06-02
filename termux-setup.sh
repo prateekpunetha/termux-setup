ff73f8dbdcea7fc8cd783f87d72711eaeb1b024a
prateekpunetha <hi@prateekpunetha.dev>
2023-06-02 12:27:12 +0530

    setup: install and setup starship
    Signed-off-by: prateekpunetha <hi@prateekpunetha.dev>

--- termux-setup.sh
+++ termux-setup.sh
@@ -2,6 +2,7 @@
 
 color_scheme_url="https://raw.githubusercontent.com/prateekpunetha/termux-setup/main/colors/colors.properties"
 font_url="https://github.com/prateekpunetha/termux-setup/raw/main/fonts/font.ttf"
+starship_config_url="https://github.com/prateekpunetha/termux-setup/raw/main/config_files/starship/config.toml"
 termux_dir="/data/data/com.termux/files/home/.termux"
 
 # termux update repos
@@ -14,13 +15,19 @@ termux-setup-storage
 yes | pkg update -y
 
 # Install main stuff
-pkg install aria2 getconf git neofetch neovim openssh python python-pip termux-api zsh -y
+pkg install aria2 getconf git neofetch neovim openssh python python-pip starship termux-api zsh -y
 
 # Install yt-dlp (youtube-dl)
 pip install yt-dlp
 
 # Change default shell to zsh
 chsh -s zsh
+
+# Setup starship
+echo "eval \$(starship init zsh)" >> $HOME/.zshrc
+
+mkdir -p $HOME/.config
+curl -so $HOME/.config/ "$starship_config_url"
 
 # Create a symbolic link for easy access to nvim and yt-dlp
 ln -s $(where nvim) /data/data/com.termux/files/usr/bin/vim
